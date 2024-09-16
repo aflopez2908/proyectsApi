5d5f1e10bc8d78c53a1e50f2bb17471a1c220a30
LopezRussiAlvaroFelipe <aflopez67@ucatolica.edu.co>
2024-09-16 18:48:09 -0500

    v8

--- InitializrSpringbootProject/src/main/java/com/crud/demo/ProductController.java
+++ InitializrSpringbootProject/src/main/java/com/crud/demo/ProductController.java
@@ -1,9 +1,18 @@
+package com.crud.demo;
+
 
 import com.crud.demo.Product;
 import com.crud.demo.ProductDAO;
 import com.crud.demo.ProductDAOImpl;
 import java.sql.SQLException;
 import java.util.List;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /*
