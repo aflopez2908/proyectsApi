dbdfe40cfb9860c7b3af4fed494e980d920794d0
LopezRussiAlvaroFelipe <aflopez67@ucatolica.edu.co>
2024-09-18 15:47:53 -0500

    actualizacion de crud

--- crud_since_0/src/main/java/com/crud_since_0/demo/controller/StudentController.java
+++ crud_since_0/src/main/java/com/crud_since_0/demo/controller/StudentController.java
@@ -8,6 +8,11 @@ import com.crud_since_0.demo.entity.Student;
 import com.crud_since_0.demo.service.StudentService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,17 +24,33 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(path= "api/v1/students")
 public class StudentController {
-
-    public StudentController() {
-        this.studentService = studentService;
-    }
     
     @Autowired
     
+    private StudentService studentService;
+    
+    @GetMapping
     public List<Student> getAll(){
-        private final StudentService studentService;
-        return 
+        
+        return studentService.getStudents();
+    }
+    
+    
+    @PostMapping
+    public void saveUpdate(@RequestBody Student student){
+        
+         studentService.saveOrUpdate(student);
+    }
+    
+    @DeleteMapping ("/{studentId}")
+    public void delete(@PathVariable("studentId") Long studentId){
+        
+        studentService.delete(studentId);
     
     }
+    
+    
+    
+    
     
 }
