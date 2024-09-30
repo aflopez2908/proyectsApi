decfcc2503dc9466911d6e16c4662b1e902c60b7
Devsleon <hardslion@gmail.com>
2024-09-30 15:10:52 -0500

    Create Entity SpringBoot - Project API

---
+++ Api/ApiGestorTiempoFreeLance/src/main/java/entity/TareasPrioridades.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+/**
+ *
+ * @author Seidor Colombia
+ */
+@Entity
+@Table(name="Tareas_Prioridades")
+public class TareasPrioridades {
+    
+}
