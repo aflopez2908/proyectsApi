f71fc72fbcada390826a6c3936b7184ee6e1a7cf
Devsleon <hardslion@gmail.com>
2024-10-14 18:24:42 -0500

    Migration 6 services + controller

---
+++ Api/ApiGestorTiempoFreeLanceV5/GestorFreeLanceV5/src/main/java/gestorfreelance/gestorfreelancev5/controllers/EmailController.java
@@ -0,0 +1,28 @@
+package gestorfreelance.gestorfreelancev5.controllers;
+import gestorfreelance.gestorfreelancev5.services.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/email")
+public class EmailController {
+    @Autowired
+    private EmailService emailService;
+    @PostMapping("/send")
+    public String sendEmail(
+            @RequestParam String to,
+            @RequestParam String subject,
+            @RequestParam String text) {
+
+        try {
+            emailService.sendSimpleMessage(to, subject, text);
+            return "Correo enviado exitosamente a " + to;
+        } catch (Exception e) {
+            return "Error al enviar el correo: " + e.getMessage();
+        }
+    }
+
+}
