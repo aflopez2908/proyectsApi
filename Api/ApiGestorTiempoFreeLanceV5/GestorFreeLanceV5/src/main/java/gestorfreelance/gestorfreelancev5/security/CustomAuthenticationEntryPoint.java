8165ca6e7b69d31d6f62255fc3930f1b2cd590d7
Devsleon <hardslion@gmail.com>
2024-11-07 17:09:16 -0500

    Modify services and new bussnes logic 3

---
+++ Api/ApiGestorTiempoFreeLanceV5/GestorFreeLanceV5/src/main/java/gestorfreelance/gestorfreelancev5/security/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,24 @@
+package gestorfreelance.gestorfreelancev5.security;
+
+import gestorfreelance.gestorfreelancev5.exception.UsuarioBloqueadoException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.core.AuthenticationException;
+
+import java.io.IOException;
+
+
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
+        // Verificar si la causa es UsuarioBloqueadoException
+        Throwable cause = authException.getCause();
+        if (cause instanceof UsuarioBloqueadoException) {
+            response.sendError(423, "Usuario bloqueado por múltiples intentos fallidos");
+        } else {
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Acceso denegado");
+        }
+    }
+}
--- Api/ApiGestorTiempoFreeLanceV5/GestorFreeLanceV5/src/main/java/gestorfreelance/gestorfreelancev5/security/JwtAuthenticationFilter.java
+++ Api/ApiGestorTiempoFreeLanceV5/GestorFreeLanceV5/src/main/java/gestorfreelance/gestorfreelancev5/security/JwtAuthenticationFilter.java
@@ -1,19 +1,15 @@
 package gestorfreelance.gestorfreelancev5.security;
 
-import gestorfreelance.gestorfreelancev5.exception.UsuarioBloqueadoException;
 import gestorfreelance.gestorfreelancev5.model.Usuario;
 import com.fasterxml.jackson.core.exc.StreamReadException;
 import com.fasterxml.jackson.databind.DatabindException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import gestorfreelance.gestorfreelancev5.service.IntentoLoginService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
@@ -31,8 +27,6 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
     public JwtAuthenticationFilter(JwtUtils jwtUtils){
         this.jwtUtils = jwtUtils;
     }
-    @Autowired
-    private IntentoLoginService intentoLoginService;
 
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request,
@@ -53,14 +47,6 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
             throw new RuntimeException(e);
         }
         System.out.println(nombre +" " + contraseña);
-        System.out.println("Se valida aqui si esta bloqueado ");
-        //Inicio modificacion
-        intentoLoginService.registrarIntento(usuario);
-        boolean bloqueado = intentoLoginService.isUsuarioBloqueado(usuario);
-        if (bloqueado) {
-            throw new UsuarioBloqueadoException("Usuario bloqueado por múltiples intentos fallidos");
-        }
-        //Fin modificacion
         UsernamePasswordAuthenticationToken authenticationToken =
                 new UsernamePasswordAuthenticationToken(nombre, contraseña);
 
@@ -92,4 +78,3 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
         super.successfulAuthentication(request, response, chain, authResult);
     }
 }
-
